04e08062cfae45a9f8d7ad7e2d89cbc9111b40ca
MwahyudinA <zudinsony@gmail.com>
2016-10-12 13:39:51 +0700

    Add files via upload

---
+++ aray5_1.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>studi kasus 5</title>
+</head>
+<body>
+	<?php
+	$shop = array
+	(
+		array(1001,"sabun lifeboy","rp.1.500,-"),
+		array(1002,"premen blaster","rp.5.600,-"),
+		array(1003,"pepsodent","rp.4.560,-"),
+		array(1004,"madu arbian","rp.30.000,-"),
+		array(1005,"kecap ABC","rp7.250,-"),
+		array(1006,"saus tomat ABC","rp.6.700,-"),
+		array(1007,"gula gulaku","rp.8.900,-"),
+		array(1008,"rinso","rp.7.100,-"),
+		array(1009,"seper pel","rp.6.450,-"),
+		array(1010,"permen tenggo","rp.5.600,-")
+		);
+
+	echo $shop[0][0]." ".$shop[0][1]." ".$shop[0][2]."<br>";
+	echo $shop[1][0]." ".$shop[1][1]." ".$shop[1][2]."<br>";
+	echo $shop[2][0]." ".$shop[2][1]." ".$shop[2][2]."<br>";
+	echo $shop[3][0]." ".$shop[3][1]." ".$shop[3][2]."<br>";
+	echo $shop[4][0]." ".$shop[4][1]." ".$shop[4][2]."<br>";
+	echo $shop[5][0]." ".$shop[5][1]." ".$shop[5][2]."<br>";
+	echo $shop[6][0]." ".$shop[6][1]." ".$shop[6][2]."<br>";
+	echo $shop[7][0]." ".$shop[7][1]." ".$shop[7][2]."<br>";
+	echo $shop[8][0]." ".$shop[8][1]." ".$shop[8][2]."<br>";
+	echo $shop[9][0]." ".$shop[9][1]." ".$shop[9][2]."<br>";
+
+	
+
+	?>
+	</body>
+	</html> 
